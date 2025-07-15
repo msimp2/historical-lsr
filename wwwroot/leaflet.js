b0a4b0c3167173da40890f5ad00c34bb0b6cc441
msimp2 <micheal.simpson@climavision.com>
2025-07-14 21:47:28 -0400

    initialized leaflet map

---
+++ wwwroot/leaflet.js
@@ -0,0 +1,8 @@
+﻿// Initialize the Leaflet map
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map('map').setView([40, -100], 6); // Centered on Kansas(ish)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+});
